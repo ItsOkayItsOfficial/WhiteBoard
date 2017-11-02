00d40bf754c73df2ca22b47a0e634d759f55c536
wcrozier12 <wcc8@nau.edu>
2017-11-02 13:57:32 -0700

    github auth working kind of

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -5,3 +5,15 @@
 * Date: 10/29/17
 * URL:  github.com/itsokayitsofficial/project2
 */
+$(function() { 
+  var authCode = getAuthCode(window.location.href);
+
+  function getAuthCode(url){
+    var error = url.match(/[&\?]error=([^&]+)/);
+    if (error) {
+        throw 'Error getting authorization code: ' + error[1];
+    }
+    return url.match(/[&\?]code=([\w\/\-]+)/)[1];
+  }
+
+});
