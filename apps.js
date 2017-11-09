bfc9342eec4c3450f52914cedba3387a25ddfc71
Alex P <alexp@itsokayitsofficial.io>
2017-11-08 18:33:53 -0700

    All

--- apps.js
+++ apps.js
@@ -11,17 +11,6 @@ const db = require('./models');
 // Express - Initialize
 const app = express();
 
-// Express - CORS
-app.use(cors())
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
-
 // Express - Static routes
 app.use("/public", express.static(__dirname + '/public'));
 
